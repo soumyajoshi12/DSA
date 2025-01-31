5c9cdbe60c6d4ef691d7e78411306295ed84d077
soumyajoshi1212 <soumya.joshi@zappian.com>
2025-02-01 00:29:52 +0530

    CHNGS

--- BubbleSort/Advquestions.js
+++ BubbleSort/Advquestions.js
@@ -24,3 +24,38 @@ const arr1 = [22, 44, 55, 1, 9, 58];
 const arr2 = [4, 8, 4, 66, 8, 20, 99];
 
 console.log(bubbleSortMerge(arr1, arr2));
+
+// 3
+const bubbleSortObject = (arr) => {
+    const key = arr.map((key) => key);
+    const n = key.length;
+  for (let i = n - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+const arr = [
+  {
+    key: 5,
+    value: "five",
+  },
+  {
+    key: 1,
+    value: "one",
+  },
+  {
+    key: 8,
+    value: "eight",
+  },
+  {
+    key: 4,
+    value: "four",
+  },
+];
+
+console.log(bubbleSortObject(arr));
